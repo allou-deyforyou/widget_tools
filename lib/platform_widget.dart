7649e079a2ee9a08f2d0e5b6df2c48a718c7128b
vodino <allou.deyforyou@gmail.com>
2023-05-11 17:50:37 +0000

    add defaultappbar

--- lib/platform_widget.dart
+++ lib/platform_widget.dart
@@ -1,6 +1,7 @@
 library platform_widget;
 
 export 'src/builder.dart';
+export 'src/defaultappbar.dart';
 export 'src/listtile.dart';
 export 'src/searchtextfield.dart';
 export 'src/textformfield.dart';
